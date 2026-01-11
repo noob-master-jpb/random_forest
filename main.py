0684f0b9876178d49673726184eacb4bea6ce210
noob-master-jpb/ <b8777051595@gmail.com>
2026-01-12 01:07:43 +0530

    .

--- main.py
+++ main.py
@@ -7,11 +7,32 @@ pio.renderers.default = "browser"
 
 df = pd.read_parquet("data.parquet")
 # pprint(dict(df.describe(include="all")))
-
-def test(input):
+# df2 = pd.read_csv("dataset/train.csv",low_memory=False)
+def filter_int(input):
+    if input is None:
+        return None
     input = input.strip("_")
-    return int(input)
+    try:
+        return float(input)
+    except:
+        return None
 
-df["Age"] = df["Age"].apply(test)
+def convert_to_binary(input):
+    if input == "Yes":
+        return 1
+    elif input == "No":
+        return 0
+    else:
+        return None
+
+
+
+df = df[(df["Age"] >= 18) & (df["Age"] <= 60)]
+df = df[(df["Num_Bank_Accounts"] <=10)]
+df = df[(df["Total_EMI_per_month"] <=200)]
+
+# print(df_filtered.describe(include="all"))
+# col = "Payment_of_Min_Amount"
+# df[col] = df2[col].apply(test2)
 
 df.to_parquet("data.parquet")
