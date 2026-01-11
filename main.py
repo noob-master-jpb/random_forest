ba7cd1b7e8567c556dbdbd2449aa4c89a6110c0a
noob-master-jpb/ <b8777051595@gmail.com>
2026-01-12 02:17:21 +0530

    Refactor data processing: replace parquet with CSV input and apply data filtering

--- main.py
+++ main.py
@@ -5,34 +5,60 @@ import plotly.io as pio
 from pprint import pprint
 pio.renderers.default = "browser"
 
-df = pd.read_parquet("data.parquet")
-# pprint(dict(df.describe(include="all")))
-# df2 = pd.read_csv("dataset/train.csv",low_memory=False)
-def filter_int(input):
-    if input is None:
+
+df = pd.read_csv("dataset/train.csv",low_memory=False)
+
+
+df = df[['Customer_ID','Month', 'Age', 'Occupation', 'Annual_Income', 'Monthly_Inhand_Salary',
+       'Num_Bank_Accounts', 'Num_Credit_Card', 'Num_of_Loan',
+       'Delay_from_due_date', 'Num_of_Delayed_Payment', 'Changed_Credit_Limit',
+       'Num_Credit_Inquiries', 'Credit_Mix', 'Outstanding_Debt',
+       'Credit_Utilization_Ratio', 'Credit_History_Age',
+       'Payment_of_Min_Amount', 'Total_EMI_per_month',
+       'Amount_invested_monthly', 'Monthly_Balance',"Credit_Score"]]
+def to_float(x):
+    if x is None:
         return None
-    input = input.strip("_")
+    if x is str:
+        x = x.strip("_")
     try:
-        return float(input)
+        return float(x)
     except:
         return None
-
-def convert_to_binary(input):
-    if input == "Yes":
-        return 1
-    elif input == "No":
-        return 0
-    else:
+    
+cols = ["Age", "Annual_Income", "Num_of_Loan","Num_of_Delayed_Payment",
+        "Changed_Credit_Limit",'Outstanding_Debt',"Amount_invested_monthly","Amount_invested_monthly","Monthly_Balance"]
+for col in cols:
+    df[col] = df[col].apply(to_float)
+    
+    
+def to_bool(x):
+    if x is None:
         return None
+    if x in 'Yes':
+        return 1
+    if x in 'No':
+        return 0
+    return None
+df["Payment_of_Min_Amount"] = df["Payment_of_Min_Amount"].apply(to_bool)
+
+df = df[(df["Age"]>=18) & (df["Age"]<=60)]
+df = df[(df["Annual_Income"]<=6000000)]
+df = df[(df["Monthly_Inhand_Salary"]<=15000)]
+df = df[(df["Num_Bank_Accounts"]<=20)]
+df = df[(df["Num_Credit_Card"]<=30)]
+df = df[(df["Num_of_Loan"]<=100) & (df["Num_of_Loan"]>=0)]
+df = df[(df["Num_of_Delayed_Payment"]<=300)]
+df = df[(df["Num_Credit_Inquiries"]<=200)]
+df = df[(df["Total_EMI_per_month"]<=500)&(df["Num_of_Delayed_Payment"]>=0)]
+df = df[(df["Amount_invested_monthly"]<=1000)]
 
 
 
-df = df[(df["Age"] >= 18) & (df["Age"] <= 60)]
-df = df[(df["Num_Bank_Accounts"] <=10)]
-df = df[(df["Total_EMI_per_month"] <=200)]
 
-# print(df_filtered.describe(include="all"))
-# col = "Payment_of_Min_Amount"
-# df[col] = df2[col].apply(test2)
 
-df.to_parquet("data.parquet")
+
+
+
+
+df.to_parquet("data.parquet", index=False)
