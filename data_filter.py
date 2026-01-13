8be939b167079d3d0844eab9c4b808251cd29d89
noob-master-jpb/ <b8777051595@gmail.com>
2026-01-13 13:10:55 +0530

    .

--- data_filter.py
+++ data_filter.py
@@ -3,13 +3,11 @@ import pandas as pd
 df = pd.read_csv("dataset/train.csv",low_memory=False)
 
 
-df = df[['Customer_ID','Month', 'Age', 'Occupation', 'Annual_Income', 'Monthly_Inhand_Salary',
+df = df[['Customer_ID','Month', 'Occupation', 'Annual_Income', 'Monthly_Inhand_Salary',
        'Num_Bank_Accounts', 'Num_Credit_Card', 'Num_of_Loan',
        'Delay_from_due_date', 'Num_of_Delayed_Payment', 'Changed_Credit_Limit',
-       'Num_Credit_Inquiries', 'Credit_Mix', 'Outstanding_Debt',
-       'Credit_Utilization_Ratio', 'Credit_History_Age',
-       'Payment_of_Min_Amount', 'Total_EMI_per_month',
-       'Amount_invested_monthly', 'Monthly_Balance',"Credit_Score"]]
+       'Num_Credit_Inquiries', 'Credit_Mix', 'Outstanding_Debt', 'Credit_History_Age',
+       'Payment_of_Min_Amount', 'Monthly_Balance',"Credit_Score"]]
 def to_float(x):
     if x is None:
         return None
@@ -20,8 +18,8 @@ def to_float(x):
     except:
         return None
     
-cols = ["Age", "Annual_Income", "Num_of_Loan","Num_of_Delayed_Payment",
-        "Changed_Credit_Limit",'Outstanding_Debt',"Amount_invested_monthly","Amount_invested_monthly","Monthly_Balance"]
+cols = [ "Annual_Income", "Num_of_Loan","Num_of_Delayed_Payment",
+        "Changed_Credit_Limit",'Outstanding_Debt',"Monthly_Balance"]
 for col in cols:
     df[col] = df[col].apply(to_float)
     
@@ -36,7 +34,7 @@ def to_bool(x):
     return None
 df["Payment_of_Min_Amount"] = df["Payment_of_Min_Amount"].apply(to_bool)
 
-df = df[(df["Age"]>=18) & (df["Age"]<=60)]
+# df = df[(df["Age"]>=18) & (df["Age"]<=60)]
 df = df[(df["Annual_Income"]<=200000)]
 df = df[(df["Monthly_Inhand_Salary"]<=15000)]
 df = df[(df["Num_Bank_Accounts"]<=20)]
@@ -44,8 +42,8 @@ df = df[(df["Num_Credit_Card"]<=12)]
 df = df[(df["Num_of_Loan"]<=12) & (df["Num_of_Loan"]>=0)]
 df = df[(df["Num_of_Delayed_Payment"]<=40)]
 df = df[(df["Num_Credit_Inquiries"]<=20)]
-df = df[(df["Total_EMI_per_month"]<=500)&(df["Num_of_Delayed_Payment"]>=0)]
-df = df[(df["Amount_invested_monthly"]<=1000)]
+# df = df[(df["Total_EMI_per_month"]<=500)&(df["Num_of_Delayed_Payment"]>=0)]
+# df = df[(df["Amount_invested_monthly"]<=1000)]
 df = df[(df["Monthly_Balance"]<=1500)]
 
 
@@ -120,18 +118,20 @@ def fill_occupation(row, cm_grp):
 df["Credit_Mix"] = df.apply(lambda x: fill_occupation(x, cm_grp), axis=1)
 df["Credit_Mix"].fillna(df['Credit_Mix'].mode()[0], inplace=True)
 
-df.drop_duplicates(inplace=True)
 
 def map_credit_score(x):
-    if x in ['Poor']:
+    if x.strip() in ['Poor','Bad']:
         return 0
-    if x in ['Standard']:
+    if x.strip() in ['Standard']:
         return 1
-    if x in ['Good']:
+    if x.strip() in ['Good']:
         return 2
     return None
 df["Credit_Score"] = df["Credit_Score"].apply(map_credit_score)
+df.drop("Customer_ID", axis=1, inplace=True)
+df["Credit_Mix"] = df["Credit_Mix"].apply(map_credit_score)
 
+df.drop_duplicates(inplace=True)
 
 df.to_parquet("data.parquet", index=False)
 df.to_csv("data.csv", index=False)
